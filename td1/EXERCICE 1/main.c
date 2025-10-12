d4754d362a0b069bfdb365e91e80178b22d1475f
arij2005 <fellaharij2@gmail.com>
2025-10-12 09:14:30 -0400

    Add files via upload

--- td1/EXERCICE 1/main.c
+++ td1/EXERCICE 1/main.c
@@ -1,3 +1,28 @@
-# ALGO-SD-TDs-Arij-
-TDs algo sd
-TD1
+#include <stdio.h>
+#include <time.h>
+#include "fibonacci.h"
+
+int main() {
+    int n = 40;
+    clock_t start, end;
+    double temps;
+
+    printf("Entrez n : ");
+    scanf("%d", &n);
+
+ 
+    start = clock();
+    int resultat_naif = fibonacci_naif(n);
+    end = clock();
+    temps = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Naif      : F(%d) = %d | Temps = %.6f secondes\n", n, resultat_naif, temps);
+
+
+    start = clock();
+    int resultat_dynamique = fibonacci_dynamique(n);
+    end = clock();
+    temps = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Dynamique : F(%d) = %d | Temps = %.6f secondes\n", n, resultat_dynamique, temps);
+
+    return 0;
+}
